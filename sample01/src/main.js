500e0299c080ef209f5003eb87c6a3f5c6d7e7e5
Atsunori Sagawa <freeman.by2018@gmail.com>
2018-12-24 01:22:26 +0900

    [modify] カスタムディレクティブの追加

--- sample01/src/main.js
+++ sample01/src/main.js
@@ -1,11 +1,27 @@
-Vue.component('my-component', {
-    props: ['items'],
-    template: '#my-template'
-})
+Vue.directive('my-directive', {
+    bind: function () {
+        console.log(this);
+        this.el.innerHTML = '<p>**bind now**</p>';
+    },
+    update: function (newValue, oldValue) {
+        this.el.innerHTML = '<p>' + oldValue + ' -> ' + newValue + '</p>';
+    },
+    unbind: function () {
+        this.el.innerHTML = '<p>**unbind**</p>';
+    },
+});
 
 function initial() {
     new Vue({
         el: '#msg',
-        data: {}
+        data: {
+            val: '',
+            message: 'this is message!'
+        },
+        methods: {
+            myfunc: function () {
+                this.message = this.val;
+            }
+        }
     });
 }
