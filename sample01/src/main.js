845aed67be821417d6f58e3a24a13e337a3fd573
Atsunori Sagawa <freeman.by2018@gmail.com>
2018-12-24 00:51:23 +0900

    [modify] Vue componentとテンプレートを追加

--- sample01/src/main.js
+++ sample01/src/main.js
@@ -1,8 +1,11 @@
+Vue.component('my-component', {
+    props: ['items'],
+    template: '#my-template'
+})
+
 function initial() {
     new Vue({
         el: '#msg',
-        data: {
-            data: []
-        }
+        data: {}
     });
 }
