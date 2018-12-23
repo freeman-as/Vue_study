b223bc95a25c65cf9b99ae2922770ece62b1ae22
Atsunori Sagawa <freeman.by2018@gmail.com>
2018-12-24 00:31:57 +0900

    [modify] v-forによる処理追加
    選択した項目を動的にリスト表示

--- sample01/src/main.js
+++ sample01/src/main.js
@@ -2,43 +2,7 @@ function initial() {
     new Vue({
         el: '#msg',
         data: {
-            msgArray: [],
-            message: '',
-            text1: '',
-            number: '0',
-            woTax: '0',
-            isA: true,
-            isB: false,
-            selF: '',
-            selB: '',
-            flg: false,
-        },
-        created: function () {
-            this.msgArray.push('sample message.');
-            this.message = this.msgArray;
-            this.selF = 'red';
-            this.selB = 'blue';
-            this.flg = true;
-    },
-        methods: {
-            doAction: function () {
-                this.msgArray.push(this.text1);
-                this.message = this.msgArray;
-            },
-            change: function () {
-                this.isA = !this.isA;
-                this.isB = !this.isB;
-            }
-        },
-        computed: {
-            wTax: {
-                get: function () {
-                    return parseInt(this.woTax * 1.08);
-                },
-                set: function (val) {
-                    this.woTax = Math.ceil(val / 1.08);
-                }
-            }
+            data: []
         }
     });
 }
