fd6990957ab6117ed5640eb24aac91a59dcb096f
Atsunori Sagawa <freeman.by2018@gmail.com>
2018-12-23 22:13:15 +0900

    [Add] Vue testファイル追加

---
+++ sample01/src/main.js
@@ -0,0 +1,15 @@
+function initial() {
+    new Vue({
+        el: '#msg',
+        data: {
+            message: 'this is Vue.js sample!',
+            text1: ''
+        },
+        methods: {
+            doAction: function () {
+                var str = this.text1;
+                this.message = 'you typed:' + str + '.';
+            }
+        }
+    })
+}
